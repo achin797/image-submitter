127ce3f9cac77525fc2fcad5a21a5bca6fd8ba34
achin797 <achintya.chandwani@gmail.com>
2025-11-12 19:47:49 +0530

    UI updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,14 +43,16 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center font-sans">
       <main
-        className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-        <form onSubmit={(formData) => {
-          formData.preventDefault();
-          const target = formData.target as HTMLFormElement;
-          const formDataObj = new FormData(target);
-          setIsDialogOpen(true);
-          return handleUpload(formDataObj);
-        }}>
+        className="flex items-center justify-center py-32 px-16 sm:items-start">
+        <form
+          className="flex flex-col items-center gap-4"
+          onSubmit={(formData) => {
+            formData.preventDefault();
+            const target = formData.target as HTMLFormElement;
+            const formDataObj = new FormData(target);
+            setIsDialogOpen(true);
+            return handleUpload(formDataObj);
+          }}>
           <Input type="file"
                  accept="image/*"
                  id="image"
